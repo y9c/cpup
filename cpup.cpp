6ab30c947d724395371203953c5ab132fe488df1
Ye Chang <yech1990@gmail.com>
2021-01-29 03:02:06 +0800

    fix format

--- cpup.cpp
+++ cpup.cpp
@@ -23,11 +23,26 @@ void usage() {
 }
 
 // global variables
-vector<string> names = {"ref",
-                        // forward strand
-                        "A", "C", "G", "T", "N", "Gap", "Insert", "Delete",
-                        // reverse strand
-                        "a", "c", "g", "t", "n", "gap", "insert", "delete"};
+vector<string> names = {
+    "ref",
+    // forward strand
+    "A",
+    "C",
+    "G",
+    "T",
+    "N",
+    "Gap",
+    "Insert",
+    "Delete",
+    // reverse strand
+    "a",
+    "c",
+    "g",
+    "t",
+    "n",
+    "gap",
+    "insert",
+    "delete"};
 
 string count_sep = ",";
 string sample_sep = "\t";
@@ -43,7 +58,7 @@ inline int str_to_num(string num) {
 
 // DS to hold the pertinent information
 class mpileup_line {
-public:
+ public:
   int pos, depth, nsample;
   string chr, ref_base;
   // Counts for different bases
@@ -54,7 +69,7 @@ public:
     depth = pos = 0;
   }
 
-  static void print_header(int nsample, ostream &out = cout) {
+  static void print_header(int nsample, ostream& out = cout) {
     out << "chr"
         << "\t"
         << "pos"
@@ -64,20 +79,26 @@ public:
         << "ref_base"
         << "\t";
     for (int i = 0; i < nsample; i++) {
-      for (const auto &n : names) {
-        out << n << count_sep;
+      for (int j = 0; j < names.size(); j++) {
+        out << names[j];
+        if (j < names.size() - 1) {
+          out << count_sep;
+        }
       }
       out << sample_sep;
     }
     out << endl;
   }
 
-  void print_mutation(ostream &out = cout) {
+  void print_mutation(ostream& out = cout) {
     out << chr << "\t" << pos << "\t" << depth << "\t" << ref_base << "\t";
     for (int i = 0; i < nsample; i++) {
       map<string, int> m = counts[i];
-      for (const auto &n : names) {
-        out << m[n] << count_sep;
+      for (int j = 0; j < names.size(); j++) {
+        out << m[names[j]];
+        if (j < names.size() - 1) {
+          out << count_sep;
+        }
       }
       out << sample_sep;
     }
@@ -86,7 +107,7 @@ public:
 };
 
 // Parse the pileup string
-map<string, int> parse_counts(string &bases, string &qual, int depth) {
+map<string, int> parse_counts(string& bases, string& qual, int depth) {
   map<string, int> m{
       {"ref", 0},    {"fwd", 0},    {"rev", 0},    {"A", 0},      {"a", 0},
       {"C", 0},      {"c", 0},      {"G", 0},      {"g", 0},      {"T", 0},
@@ -99,137 +120,137 @@ map<string, int> parse_counts(string &bases, string &qual, int depth) {
     string indelsize_string;
     int indelsize_int = 0;
     switch (base) {
-    // Match to reference
-    case '.':
-      m["fwd"] += 1;
-      break;
-    case ',':
-      m["rev"] += 1;
-      break;
-    case 'a':
-      m["a"] += 1;
-      break;
-    case 'A':
-      m["A"] += 1;
-      break;
-    case 'c':
-      m["c"] += 1;
-      break;
-    case 'C':
-      m["C"] += 1;
-      break;
-    case 'g':
-      m["g"] += 1;
-      break;
-    case 'G':
-      m["G"] += 1;
-      break;
-    case 't':
-      m["t"] += 1;
-      break;
-    case 'T':
-      m["T"] += 1;
-      break;
-    case 'n':
-      m["n"] += 1;
-      break;
-    case 'N':
-      m["N"] += 1;
-      break;
-    // This base is a gap (--reverse-del suport)
-    // similar with Deletecount and deletecount, but with some difference
-    case '*':
-      m["Gap"] += 1;
-      break;
-    case '#':
-      m["gap"] += 1;
-      break;
-    // Insertion
-    case '+':
-      i++;
-      // 48 is number '0', 57 is number '9'
-      while (bases[i] >= 48 && bases[i] <= 57) {
-        indelsize_string = indelsize_string + bases[i];
+      // Match to reference
+      case '.':
+        m["fwd"] += 1;
+        break;
+      case ',':
+        m["rev"] += 1;
+        break;
+      case 'a':
+        m["a"] += 1;
+        break;
+      case 'A':
+        m["A"] += 1;
+        break;
+      case 'c':
+        m["c"] += 1;
+        break;
+      case 'C':
+        m["C"] += 1;
+        break;
+      case 'g':
+        m["g"] += 1;
+        break;
+      case 'G':
+        m["G"] += 1;
+        break;
+      case 't':
+        m["t"] += 1;
+        break;
+      case 'T':
+        m["T"] += 1;
+        break;
+      case 'n':
+        m["n"] += 1;
+        break;
+      case 'N':
+        m["N"] += 1;
+        break;
+      // This base is a gap (--reverse-del suport)
+      // similar with Deletecount and deletecount, but with some difference
+      case '*':
+        m["Gap"] += 1;
+        break;
+      case '#':
+        m["gap"] += 1;
+        break;
+      // Insertion
+      case '+':
+        i++;
+        // 48 is number '0', 57 is number '9'
+        while (bases[i] >= 48 && bases[i] <= 57) {
+          indelsize_string = indelsize_string + bases[i];
+          i = i + 1;
+        }
+        if (isupper(bases[i])) {
+          m["Insert"] += 1;
+        } else {
+          m["insert"] += 1;
+        }
+        indelsize_int = str_to_num(indelsize_string);
+        i += indelsize_int - 1;
+        break;
+      // Deletion
+      case '-':
+        i++;
+        // 48 is number '0', 57 is number '9'
+        while (bases[i] >= 48 && bases[i] <= 57) {
+          indelsize_string = indelsize_string + bases[i];
+          i = i + 1;
+        }
+        if (isupper(bases[i])) {
+          m["Delete"] += 1;
+        } else {
+          m["delete"] += 1;
+        }
+        indelsize_int = str_to_num(indelsize_string);
+        i += indelsize_int - 1;
+        break;
+      // Reference skips
+      case '<':
+      case '>':
+        break;
+      // End of read segment
+      case '$':
+        break;
+      // Beginning of read segment, Skip
+      case '^':
         i = i + 1;
-      }
-      if (isupper(bases[i])) {
-        m["Insert"] += 1;
-      } else {
-        m["insert"] += 1;
-      }
-      indelsize_int = str_to_num(indelsize_string);
-      i += indelsize_int - 1;
-      break;
-    // Deletion
-    case '-':
-      i++;
-      // 48 is number '0', 57 is number '9'
-      while (bases[i] >= 48 && bases[i] <= 57) {
-        indelsize_string = indelsize_string + bases[i];
-        i = i + 1;
-      }
-      if (isupper(bases[i])) {
-        m["Delete"] += 1;
-      } else {
-        m["delete"] += 1;
-      }
-      indelsize_int = str_to_num(indelsize_string);
-      i += indelsize_int - 1;
-      break;
-    // Reference skips
-    case '<':
-    case '>':
-      break;
-    // End of read segment
-    case '$':
-      break;
-    // Beginning of read segment, Skip
-    case '^':
-      i = i + 1;
-      break;
-    default:
-      string err = "Unknown ref base: ";
-      err += base;
-      throw runtime_error(err);
+        break;
+      default:
+        string err = "Unknown ref base: ";
+        err += base;
+        throw runtime_error(err);
     }
   }
 
   m["ref"] = m["fwd"] + m["rev"];
-  // Set the appropriate count for ref nucleotide
 
   return m;
 }
 
-map<string, int> adjust_counts(map<string, int> m, string &ref_base) {
+// Set the appropriate count for ref nucleotide
+map<string, int> adjust_counts(map<string, int> m, string& ref_base) {
   switch (ref_base[0]) {
-  case 'A':
-  case 'a':
-    m["A"] = m["fwd"];
-    m["a"] = m["rev"];
-    break;
-  case 'C':
-  case 'c':
-    m["C"] = m["fwd"];
-    m["c"] = m["rev"];
-    break;
-  case 'G':
-  case 'g':
-    m["G"] = m["fwd"];
-    m["g"] = m["rev"];
-    break;
-  case 'T':
-  case 't':
-    m["T"] = m["fwd"];
-    m["t"] = m["rev"];
-    break;
-  case 'N':
-  case 'n':
-    m["N"] = m["fwd"];
-    m["n"] = m["rev"];
-    break;
-  // Deal with -,R,Y,K,M,S,W etc
-  default:
-    break;
+    case 'A':
+    case 'a':
+      m["A"] = m["fwd"];
+      m["a"] = m["rev"];
+      break;
+    case 'C':
+    case 'c':
+      m["C"] = m["fwd"];
+      m["c"] = m["rev"];
+      break;
+    case 'G':
+    case 'g':
+      m["G"] = m["fwd"];
+      m["g"] = m["rev"];
+      break;
+    case 'T':
+    case 't':
+      m["T"] = m["fwd"];
+      m["t"] = m["rev"];
+      break;
+    case 'N':
+    case 'n':
+      m["N"] = m["fwd"];
+      m["n"] = m["rev"];
+      break;
+    // Deal with -,R,Y,K,M,S,W etc
+    default:
+      break;
   }
   return m;
 }
@@ -273,7 +294,7 @@ mpileup_line process_mpileup_line(string line) {
   return ml;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   for (int i = 1; i < argc; i++) {
     if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
       usage();
@@ -287,7 +308,7 @@ int main(int argc, char *argv[]) {
   try {
     mpileup_line ml = process_mpileup_line(line);
     mpileup_line::print_header(ml.nsample);
-  } catch (const std::runtime_error &e) {
+  } catch (const std::runtime_error& e) {
     cerr << e.what() << endl;
     cerr << "\nError parsing line " << line;
   }
@@ -297,7 +318,7 @@ int main(int argc, char *argv[]) {
     try {
       mpileup_line ml = process_mpileup_line(line);
       ml.print_mutation();
-    } catch (const std::runtime_error &e) {
+    } catch (const std::runtime_error& e) {
       cerr << e.what() << endl;
       cerr << "\nError parsing line " << line;
       break;
